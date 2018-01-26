5046fa05e88d00e6972c6e5266772234926a462f
demiralp <demiralp@vr.rwth-aachen.de>
2018-01-26 16:54:33 +0100

    Framegraph progress.

--- include/fg/framegraph.hpp
+++ include/fg/framegraph.hpp
@@ -1,11 +1,33 @@
 #ifndef FG_FRAMEGRAPH_HPP_
 #define FG_FRAMEGRAPH_HPP_
 
+#include <memory>
+
+#include <boost/graph/directed_graph.hpp>
+
+#include <fg/render_resource.hpp>
+#include <fg/render_task_base.hpp>
+
 namespace fg
 {
-class framegraph
+class node;
+
+class framegraph : public boost::directed_graph<std::shared_ptr<node>>
 {
-  
+public:
+  using graph_type        = boost::directed_graph<std::shared_ptr<node>>;
+  using vertex_descriptor = vertex_descriptor;
+  using edge_descriptor   = edge_descriptor  ;
+
+  framegraph           ()                        = default;
+  framegraph           (const framegraph&  that) = default;
+  framegraph           (      framegraph&& temp) = default;
+  virtual ~framegraph  ()                        = default;
+  framegraph& operator=(const framegraph&  that) = default;
+  framegraph& operator=(      framegraph&& temp) = default;
+
+protected:
+
 };
 }
 
